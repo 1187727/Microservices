d67e73151cd0a2fe7357785ae96f859640862ee4
ANANTH  T <596091@India.TCS.com>
2018-03-23 14:44:17 +0530

    springsecurity

---
+++ SpringinMemorySecurity/src/main/java/com/demo/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+	@GetMapping("/name")
+	public String getName() {
+		return "Security Works...!";		
+	}
+	
+	
+	
+	
+	
+}
---
+++ SpringinMemorySecurity/src/main/java/com/demo/security/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.demo.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig  extends WebSecurityConfigurerAdapter{
+	//Generating Encrypted Password
+	/*public static void main() {
+		System.out.println(new BCryptPasswordEncoder().encode("admin"));
+	}*/
+
+	
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth)throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("mano")
+		.password("$2a$10$aR9MYOxT7uWtvlB9K37mGeAnArs8sUGBFxe6/vn9aF6hz4SR52x46")
+		.roles("ADMIN")
+		.and()
+		.passwordEncoder(new BCryptPasswordEncoder());
+	
+}
+	@Override
+	protected void configure(HttpSecurity http)throws Exception{
+		http.csrf().disable().httpBasic()
+		.and()
+		.authorizeRequests()
+		.antMatchers("/name").hasAnyRole("ADMIN")
+		.anyRequest().authenticated();
+		
+		
+	}
+		
+	}
+
